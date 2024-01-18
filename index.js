fafce063f73afe68cccb1c4d35fc5b3d68c6cc38
Juan Hoyos <juanhoyos1347@gmail.com>
2024-01-17 23:21:33 -0500

    menu principal

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 #!/usr/bin/env node
-const commander = require('commander');
+import select from '@inquirer/select';
 
-const program = new commander.Command();
 
-program
-    .command('generate')
+const answer = await select({
+    message: 'Generate modules for',
+    choices: [
+        {
+            name: 'Architecture',
+            value: 1,
+        },
+        {
+            name: 'Database',
+            value: 2,
+        },
+        {
+            name: 'Documentation',
+            value: 3,
+        },
+        {
+            name: 'Exit',
+            value: 4,
+        },
+    ],
+});
 
-program.parse(process.argv);
-console.log('Hola');
+
