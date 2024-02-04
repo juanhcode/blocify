2db45180b8ba83371da61ef0fa1e45a2bb603a1f
GregoryG8 <gregory.gonzalez@correounivalle.edu.co>
2024-02-04 15:05:35 -0500

    Created MVC Route function

--- createMVCFolders.js
+++ createMVCFolders.js
@@ -1,23 +1,53 @@
 import path from "path";
 import fs from "fs/promises";
+import { createRoute } from "./createMVCRoutes.js";
 
 const folderNames = ["controllers", "services", "routes"];
 
 const createFolder = async () => {
-  const currentDirectory = process.cwd();
+    const currentDirectory = process.cwd();
+    const srcFolderPath = path.join(currentDirectory, "src");
 
-  await Promise.all(
-    folderNames.map(async (folders) => {
-      const folderPath = path.join(currentDirectory, folders);
+    console.log("srcFolderPath:", srcFolderPath);
 
-      try {
-        await fs.mkdir(folderPath);
-        console.log("Folder was created in: ", folderPath);
-      } catch (err) {
-        console.error("Could not create folder: ", err);
-      }
-    })
-  );
+    try {
+        await fs.access(srcFolderPath);
+    } catch (err) {
+        console.log("'src' folder does not exist. Creating folders in the current directory.");
+
+        await Promise.all(
+            folderNames.map(async (folder) => {
+                const folderPath = path.join(currentDirectory, folder);
+
+                try {
+                    await fs.mkdir(folderPath);
+                    console.log("Folder was created at: ", folderPath);
+                } catch (err) {
+                    console.error("Could not create folder: ", err);
+                }
+            })
+        );
+
+        // Invocar la función createRoute después de crear las carpetas
+        await createRoute();
+        return;
+    }
+
+    await Promise.all(
+        folderNames.map(async (folder) => {
+            const folderPath = path.join(srcFolderPath, folder);
+
+            try {
+                await fs.mkdir(folderPath);
+                console.log("Folder was created at: ", folderPath);
+            } catch (err) {
+                console.error("Could not create folder: ", err);
+            }
+        })
+    );
+
+    // Invocar la función createRoute después de crear las carpetas
+    await createRoute();
 };
 
 export { createFolder };
---
+++ createMVCRoutes.js
@@ -0,0 +1,58 @@
+import fs from "fs/promises";
+import path from "path";
+
+const contentRoute = `// Hola, soy la monda parada`;
+
+const createRoute = async () => {
+  const currentDirectory = process.cwd();
+  const srcFolderPath = path.join(currentDirectory, "src");
+  const routesFolderPath = path.join(currentDirectory, "routes");
+  const routesFolderPath2 = path.join(srcFolderPath, "routes");
+  const routeFilePath = path.join(routesFolderPath, "route.js");
+  const routeFilePath2 = path.join(routesFolderPath2, "route.js");
+
+  console.log("routesFolderPath:", routesFolderPath);
+  console.log("routeFilePath:", routeFilePath);
+
+  try {
+    await fs.access(srcFolderPath);
+  } catch (err) {
+    console.error(
+        "'src' folder does not exist. Please create the 'src' folder first."
+      );
+
+      try {
+        await fs.access(routesFolderPath);
+      } catch (err) {
+        console.error(
+          "'routes' folder does not exist. Please create the 'routes' folder first."
+        );
+        return;
+      }
+    
+      try {
+        await fs.writeFile(routeFilePath, contentRoute);
+        console.log(`Successfully created file: "${routeFilePath}"`);
+      } catch (err) {
+        console.error(`Error writing file ${routeFilePath}: ${err}`);
+      }
+  }
+
+  try {
+    await fs.access(routesFolderPath2);
+  } catch (err) {
+    console.error(
+      "'routes' folder does not exist. Please create the 'routes' folder first."
+    );
+    return;
+  }
+
+  try {
+    await fs.writeFile(routeFilePath2, contentRoute);
+    console.log(`Successfully created file: "${routeFilePath2}"`);
+  } catch (err) {
+    console.error(`Error writing file ${routeFilePath2}: ${err}`);
+  }
+};
+
+export { createRoute };
