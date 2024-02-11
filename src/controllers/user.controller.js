f83a6ccbff01786a173f4f8b8d5c7c17fb4f11bb
Juan Hoyos <juanhoyos1347@gmail.com>
2024-02-10 23:01:25 -0500

    custom config

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,20 @@
+
+    // create
+    const createuserController = (req, res) => {
+
+    };
+
+    // get
+    const getuserController = (req, res) => {
+
+    };
+    
+    // delete
+    const deleteuserController = (req, res) => {
+
+    };
+    
+    // update
+    const updateuserController = (req, res) => {
+
+    };
