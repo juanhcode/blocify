44c21bb9578566e80128af557ef228924e5d6c00
Juan Hoyos <juanhoyos1347@gmail.com>
2024-02-10 22:43:17 -0500

    method http on menu

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
 #!/usr/bin/env node
 import select from '@inquirer/select';
-import {form,generateArchitecture} from './generateArchitecture.js';
-import { createFolder } from './createMVCFolders.js';
+import { form, generateArchitecture } from './generateArchitecture.js';
 
-const answer = await select({
-    message: 'Generate modules for',
-    choices: [
-        {
-            name: 'Architecture',
-            value: 1,
-        },
-        {
-            name: 'Database',
-            value: 2,
-        },
-        {
-            name: 'Documentation',
-            value: 3,
-        },
-        {
-            name: 'Exit',
-            value: 4,
-        },
-    ],
-});
-
-switch (answer) {
+const menu = async () => {
+    const answer = await select({
+        message: 'Generate modules for',
+        choices: [
+            {
+                name: 'Architecture',
+                value: 1,
+            },
+            {
+                name: 'Database',
+                value: 2,
+            },
+            {
+                name: 'Documentation',
+                value: 3,
+            },
+            {
+                name: 'Exit',
+                value: 4,
+            },
+        ],
+    });
+    return answer;
+}
+const number = await menu();
+switch (number) {
     case 1:
-        const option =  await generateArchitecture();
-        console.log(option);
-        switch(option){
+        const option = await generateArchitecture();
+        switch (option) {
             case 1:
                 await form();
                 break;
             case 4:
-                
-
+                await menu();
+                break;
+            default:
+                break;
         }
-
         break;
     default:
         break;
--- routes/user.route.js
+++
@@ -1,1 +0,0 @@
-// Hola, soy la monda parada
